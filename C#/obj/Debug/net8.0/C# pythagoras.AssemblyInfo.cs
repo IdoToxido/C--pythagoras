62cb1bf24f123875b6cfd8eb0f89365ba65eaafc
IdoToxido <edowaizman@gmail.com>
2024-03-03 21:10:13 +0200

    bullshit

--- C#/obj/Debug/net8.0/C# pythagoras.AssemblyInfo.cs
+++ C#/obj/Debug/net8.0/C# pythagoras.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# pythagoras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7643e79d7929ad72f44553b8ade2d5da47cda3e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# pythagoras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# pythagoras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
